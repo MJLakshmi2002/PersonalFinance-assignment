fdbd5be67d0880338a8884df938bd53469f21b4a
Lakshmi MJ <mjlakshmi66@gmail.com>
2025-02-11 16:04:58 +0530

    intial commit

--- src/main/java/com/assignment/project_crud/model/FinanceEntry.java
+++ src/main/java/com/assignment/project_crud/model/FinanceEntry.java
@@ -1,4 +1,26 @@
 package com.assignment.project_crud.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class FinanceEntry {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private double amount;
+    private String category;
+    private LocalDate date;
+    private String description;
+    private String type; // "income" or "expense"
 }
+
